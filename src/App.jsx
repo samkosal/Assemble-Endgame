1d34e9c9d75c7eaa1d9becfd8b096b5ce31f4e51
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-15 21:25:09 -0700

    created home page with win condition

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,18 @@ import React from "react"
  * 
  */
 
-export default function Hangman() {
+export default function AssemblyEndgame() {
     return (
         <main>
-            Game goes here
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p> guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p> Well done!</p>
+            </section>
         </main>
     )
 }
