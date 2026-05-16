88c7f7a6918b30cc444625368446f57ad8f42b54
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-15 21:48:06 -0700

    added database, added a new section for mapping through language data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from "react"
-
-/**
- * Project planning:
- * 
- * Questions to ask yourself before writing any code:
- * 
- * - What are the main containers of elements I need
- *   in this app?
- * 
- * 
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * 
- * 
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * 
- * 
- */
-
+import {languages} from "./languages"
 export default function AssemblyEndgame() {
+
+
     return (
         <main>
             <header>
@@ -27,9 +10,24 @@ export default function AssemblyEndgame() {
                 <p> guess the word within 8 attempts to keep the
                 programming world safe from Assembly!</p>
             </header>
+
             <section className="game-status">
                 <h2>You win!</h2>
                 <p> Well done!</p>
+            </section>
+
+            <section className="language-chips">
+                {languages.map(language => (
+                    <span 
+                        className="chip"
+                        key={language.name}
+                        style={{
+                            backgroundColor: language.backgroundColor,
+                            color: language.color
+                    }}>
+                        {language.name}
+                    </span>
+                ))}
             </section>
         </main>
     )
