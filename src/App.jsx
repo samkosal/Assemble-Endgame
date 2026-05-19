0f6c21f59b75e41dd53c46e1a8d5550d9bcd7641
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-19 12:23:19 -0700

    added button color change depending on if seletected word matches the word to guess

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { languages } from "./languages"
+import { clsx } from "clsx"
 export default function AssemblyEndgame() {
 
     const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    console.log(guessedLetters)
+
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button key={letter}>{letter.toUpperCase()}</button>
-    ))
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters => 
+            prevLetters.includes(letter) ? 
+                prevLetters : 
+                [...prevLetters, letter]
+        )
+    }
+
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+
+        })
+
+        console.log(className)
+        return (
+            <button 
+                className={className}
+                key={letter} 
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button> 
+        ) 
+    })
     // learn something new 
     // array of spans → render directly
-    // array of letters → map to spans before rendering
+    // array of letters → map to spans before rendering 
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
     ))
@@ -55,6 +85,7 @@ export default function AssemblyEndgame() {
             <section className="keyboard">
                 {keyboardElements}
             </section>
+            <button className="new-game">New Game</button>
         </main>
     )
 }
